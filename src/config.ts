c42689bf8efc1e95879ca75da120237873f206eb
Sarthak Bhuptani <mrsarthak825@example.com>
2026-01-06 16:50:12 +0530

    chnage

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,3 @@
-export const API_URL = "http://localhost:5000/api/menu";
+export const API_URL = import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api/menu"
+    : "https://mom-sspecial.onrender.com/api/menu";
